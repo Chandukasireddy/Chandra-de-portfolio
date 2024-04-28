b21c785da934dd4cba93b3bb69d41b7de7e0bd56
Chandukasireddy <chandrakiranreddy2406@gmail.com>
2024-04-28 14:48:53 +0530

    back to my email

--- src/assets/lib/data.tsx
+++ src/assets/lib/data.tsx
@@ -311,7 +311,7 @@ export const FooterLinks = [
 
 export const sideBarRightMail = {
   link: "mailto:chandukasireddy02@gmail.com",
-  text: "chandukasireddy02@gmail.com",
+  text: "myemail@com",
 } as const;
 
 export const sideBarLeftSocials = [
