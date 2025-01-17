6288451b0961ff73fae62a1463f3c696c3674bc7
Chandrakiran Reddy Kasireddy <chandrakiranreddy2406@gmail.com>
2025-01-17 19:15:06 +0000

    contact me..

--- src/assets/lib/data.tsx
+++ src/assets/lib/data.tsx
@@ -311,7 +311,7 @@ export const FooterLinks = [
 
 export const sideBarRightMail = {
   link: "mailto:chandukasireddy02@gmail.com",
-  text: "Contactme",
+  text: "Contact-me..<-",
 } as const;
 
 export const sideBarLeftSocials = [
